599d9aefe53be01894a5aae19a2656a37c0c987d
SteveHouren <shouren@umich.edu>
2018-11-23 16:44:51 -0500

    Modified database initializer file and changed placeholder on reg form from Name to First Name

--- src/main/resources/static/DataInit/DataInit.sql
+++ src/main/resources/static/DataInit/DataInit.sql
@@ -30,7 +30,9 @@ CREATE TABLE `user_role` (
   `user_id` int(11) NOT NULL,
   `role_id` int(11) NOT NULL,
   PRIMARY KEY (`user_id`,`role_id`),
-  KEY `FKa68196081fvovjhkek5m97n3y` (`role_id`),
-  CONSTRAINT `FK859n2jvi8ivhui0rl0esws6o` FOREIGN KEY (`user_id`) REFERENCES `user` (`user_id`),
-  CONSTRAINT `FKa68196081fvovjhkek5m97n3y` FOREIGN KEY (`role_id`) REFERENCES `role` (`role_id`)
+  KEY `user_role_key` (`role_id`),
+  CONSTRAINT `user_userrole` FOREIGN KEY (`user_id`) REFERENCES `user` (`user_id`),
+  CONSTRAINT `role_userrole` FOREIGN KEY (`role_id`) REFERENCES `role` (`role_id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+INSERT INTO `role` VALUES (1,'ADMIN');
